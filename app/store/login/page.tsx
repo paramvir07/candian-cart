d4e96aa133c3e893f6c394181959d80378e307ec
Paramvir Singh Thind <paramvirsingh2540@gmail.com>
2026-02-08 01:24:21 -0800

    customer auth

--- app/store/login/page.tsx
+++ app/store/login/page.tsx
@@ -1,10 +1,15 @@
 import { LoginForm } from "@/components/auth/login-form";
 
+// login user name and pass -: paramvirsingh2540@gmail.com, param@123
+// actions/aut/auth.action.ts
+
+// For store signup, got to actions/auth/storeSignup.actiond.ts
+
 export default function Page() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <LoginForm role="shop" />
+        <LoginForm role="store" />
       </div>
     </div>
   );
